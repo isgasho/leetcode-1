f5fc66ca5d311fd656feb96d4bdc99ba1cabd5f1
Yue Yang <g1enyy0ung@gmail.com>
2019-01-13 12:51:53 +0800

    Add 463

--- src/index_461_470/mod.rs
+++ src/index_461_470/mod.rs
@@ -6,3 +6,21 @@ pub fn hamming_distance(x: i32, y: i32) -> i32 {
         .collect::<String>()
         .len() as i32;
 }
+
+// 463
+pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+    let mut island = 0;
+    let mut repeat = 0;
+
+    for (i, row) in grid.iter().enumerate() {
+        for (j, &item) in row.iter().enumerate() {
+            if item == 1 {
+                island += 1;
+                repeat += if i != 0 && grid[i - 1][j] == 1 { 1 } else { 0 };
+                repeat += if j != 0 && grid[i][j - 1] == 1 { 1 } else { 0 };
+            }
+        }
+    }
+
+    return island * 4 - repeat * 2;
+}
--- tests/index_461_470_test.rs
+++ tests/index_461_470_test.rs
@@ -6,3 +6,20 @@ use leetcode::index_461_470::*;
 fn hamming_distance_works() {
     assert_eq!(hamming_distance(1, 4), 2);
 }
+
+#[test]
+fn island_perimeter_works() {
+    assert_eq!(
+        island_perimeter(vec![
+            vec![0, 1, 0, 0],
+            vec![1, 1, 1, 0],
+            vec![0, 1, 0, 0],
+            vec![1, 1, 0, 0]
+        ]),
+        16
+    );
+    assert_eq!(island_perimeter(vec![vec![1]]), 4);
+    assert_eq!(island_perimeter(vec![vec![1, 0]]), 4);
+    assert_eq!(island_perimeter(vec![vec![0, 1]]), 4);
+    assert_eq!(island_perimeter(vec![vec![0, 0, 0, 1]]), 4);
+}
