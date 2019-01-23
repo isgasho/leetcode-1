5a5a84995c178285a9bd151e77040bc356d161aa
Yue Yang <g1enyy0ung@gmail.com>
2019-01-23 23:55:49 +0800

    Add 371

---
+++ src/index_371_380/mod.rs
@@ -0,0 +1,4 @@
+// 371
+pub fn get_sum(a: i32, b: i32) -> i32 {
+    vec![a, b].iter().sum()
+}
---
+++ tests/index_371_380_test.rs
@@ -0,0 +1,9 @@
+extern crate leetcode;
+
+use leetcode::index_371_380::*;
+
+// 371
+#[test]
+fn get_sum_works() {
+    assert_eq!(get_sum(2, 3), 5);
+}
