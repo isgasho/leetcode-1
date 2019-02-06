8aabf4261a00ff1253a8ddf631cddeff329f1003
Yue Yang <g1enyy0ung@gmail.com>
2019-02-06 11:18:17 +0800

    Add 383

--- src/index_381_390/mod.rs
+++ src/index_381_390/mod.rs
@@ -1,5 +1,25 @@
 use std::collections::HashMap;
 
+// 383
+pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+    let mut store = vec![0; 26];
+
+    for c in magazine.chars() {
+        let i = c as usize;
+        store[i - 97] += 1;
+    }
+
+    for c in ransom_note.chars() {
+        let i = c as usize;
+        if store[i - 97] == 0 {
+            return false;
+        }
+        store[i - 97] -= 1;
+    }
+
+    return true;
+}
+
 // 389
 pub fn find_the_difference(s: String, t: String) -> char {
     let mut store = HashMap::new();
--- tests/index_381_390_test.rs
+++ tests/index_381_390_test.rs
@@ -2,6 +2,13 @@ extern crate leetcode;
 
 use leetcode::index_381_390::*;
 
+// 383
+#[test]
+fn can_construct_works() {
+    assert_eq!(can_construct(String::from("aa"), String::from("aab")), true);
+    assert_eq!(can_construct(String::from("aa"), String::from("ab")), false);
+}
+
 // 389
 #[test]
 fn find_the_difference_works() {
