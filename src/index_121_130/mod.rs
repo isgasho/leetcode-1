f66cd47d55acfcdfb1743afa04d2770aadc02f49
Yue Yang <g1enyy0ung@gmail.com>
2019-02-09 17:23:52 +0800

    Add 121

--- src/index_121_130/mod.rs
+++ src/index_121_130/mod.rs
@@ -1,5 +1,21 @@
+use std::cmp;
+
+// 121
+pub fn max_profit_121(prices: Vec<i32>) -> i32 {
+    let mut max = 0;
+    let mut max_cur = 0;
+
+    for i in 1..prices.len() {
+        max_cur += prices[i] - prices[i - 1];
+        max_cur = cmp::max(0, max_cur);
+        max = cmp::max(max, max_cur);
+    }
+
+    max
+}
+
 // 122
-pub fn max_profit(prices: Vec<i32>) -> i32 {
+pub fn max_profit_122(prices: Vec<i32>) -> i32 {
     let mut profit = 0;
 
     for i in 1..prices.len() {
--- tests/index_121_130_test.rs
+++ tests/index_121_130_test.rs
@@ -2,9 +2,16 @@ extern crate leetcode;
 
 use leetcode::index_121_130::*;
 
+// 121
+#[test]
+fn max_profit_121_works() {
+    assert_eq!(max_profit_121(vec![7, 1, 5, 3, 6, 4]), 5);
+    assert_eq!(max_profit_121(vec![1, 2, 3, 4, 5]), 4);
+}
+
 // 122
 #[test]
-fn max_profit_works() {
-    assert_eq!(max_profit(vec![7, 1, 5, 3, 6, 4]), 7);
-    assert_eq!(max_profit(vec![1, 2, 3, 4, 5]), 4);
+fn max_profit_122_works() {
+    assert_eq!(max_profit_122(vec![7, 1, 5, 3, 6, 4]), 7);
+    assert_eq!(max_profit_122(vec![1, 2, 3, 4, 5]), 4);
 }
