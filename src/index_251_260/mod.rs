fdd2e67a1ad6b8b58146ef22ab121f7c15738de9
Yue Yang <g1enyy0ung@gmail.com>
2019-02-17 15:16:39 +0800

    Add 257

--- src/index_251_260/mod.rs
+++ src/index_251_260/mod.rs
@@ -1,3 +1,44 @@
+use super::helper::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// 257
+pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
+    let mut result: Vec<String> = vec![];
+
+    fn helper(root: Option<Rc<RefCell<TreeNode>>>, middle: String, res: &mut Vec<String>) {
+        if root == None {
+            return;
+        }
+
+        match root {
+            None => return,
+            Some(x) => {
+                let left = x.borrow().left.clone();
+                let right = x.borrow().right.clone();
+
+                if left == None && right == None {
+                    res.push(middle.clone() + &x.borrow().val.to_string());
+                }
+                helper(
+                    left,
+                    middle.clone() + &x.borrow().val.to_string() + &"->",
+                    res,
+                );
+                helper(
+                    right,
+                    middle.clone() + &x.borrow().val.to_string() + &"->",
+                    res,
+                );
+            }
+        }
+    }
+
+    helper(root, "".to_string(), &mut result);
+
+    return result;
+}
+
 // 258
 pub fn add_digits(num: i32) -> i32 {
     (num - 1) % 9 + 1
--- tests/index_251_260_test.rs
+++ tests/index_251_260_test.rs
@@ -2,6 +2,13 @@ extern crate leetcode;
 
 use leetcode::index_251_260::*;
 
+// 257
+#[test]
+fn binary_tree_paths_works() {
+    let test_result: Vec<String> = vec![];
+    assert_eq!(binary_tree_paths(None), test_result);
+}
+
 // 258
 #[test]
 fn invert_tree_works() {
