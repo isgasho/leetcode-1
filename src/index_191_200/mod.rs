e401b37d4ed0d818c4adc72e5e72497b4011a4df
Yue Yang <g1enyy0ung@gmail.com>
2019-02-23 21:19:00 +0800

    Fixes 198

--- src/index_191_200/mod.rs
+++ src/index_191_200/mod.rs
@@ -1,8 +1,19 @@
+use std::cmp;
+
 // 198
 pub fn rob(nums: Vec<i32>) -> i32 {
+    if nums.len() == 0 {
+        return 0;
+    }
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    if nums.len() == 2 {
+        return cmp::max(nums[0], nums[1]);
+    }
     let mut store = vec![0; nums.len()];
     store[0] = nums[0];
-    store[1] = nums[1];
+    store[1] = cmp::max(nums[0], nums[1]);
 
     for i in 2..nums.len() {
         store[i] = if nums[i] + store[i - 2] > store[i - 1] {
